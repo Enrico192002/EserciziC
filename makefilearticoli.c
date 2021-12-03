9d414410dcebd87c3b80039c4b0c936d17be479b
Enrico192002 <94382848+Enrico192002@users.noreply.github.com>
2021-12-03 16:08:30 +0100

    Update makefilearticoli.c

--- makefilearticoli.c
+++ makefilearticoli.c
@@ -4,38 +4,31 @@
 
 int main(int argc, char *argv[])
 {
-
-char riga[31];
-char file; 
-int count=0;
- 
-
+    int dimensione=argv[2];
+    char riga[dimensione];
+    File *file;
+    int x=0;
     if(argc<2)
     {
-        printf("inserire nome file\n");
+        printf("Non ci sono abbastanza parametri, controlla di aver messo il nome del file\n");
         exit(-1);
     }
-
-
     if(argc<3)
     {
-        printf("inserire il numero massimo di caratteri per riga\n");
+        printf("Non ci sono abbastanza parametri, controlla di aver messo il numero massimo di caratteri per riga\n");
         exit(-2);
     }
-
-
     file=fopen(argv[1],"r");
     if(file==NULL)
     {
-        printf("il file %s non esiste\n",argv[1]);  //controlla se il file non esiste
+        printf("il file %s non esiste nella posizione indicata\n",argv[1]);
         exit(-3);
     }
-
     while(fgets(riga,sizeof(riga),file)!=NULL)
     {
         printf("%s\n",riga);
-        count=++1;
+        x=x+1;
     }
     fclose(file);
-    return count;
+    return x;
 }
