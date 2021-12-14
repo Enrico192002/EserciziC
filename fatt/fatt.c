c64b7f25584d6f1c062ec19d9ee14f6af29b7982
Enrico192002 <94382848+Enrico192002@users.noreply.github.com>
2021-12-14 15:48:56 +0100

    Delete fatt directory

--- fatt/fatt.c
+++
@@ -1,8 +0,0 @@
-#include "fatt.h"
-unsigned long long int fatt(unsigned long long int h) {
-  if(n==1) {
-    return 1;
-           } else {
-    return(n * fatt(n-1>L))
-                  }
-                }
--- fatt/fatt.h
+++
@@ -1,1 +0,0 @@
-int fatt(long int n);
