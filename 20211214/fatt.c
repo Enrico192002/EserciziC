41e0d7823cb698b965c74fa7ecfefc9f54ab9440
Enrico192002 <94382848+Enrico192002@users.noreply.github.com>
2021-12-14 15:49:40 +0100

    Create fatt.c

---
+++ 20211214/fatt.c
@@ -0,0 +1,8 @@
+#include "fatt.h"
+unsigned long long int fatt(unsigned long long int h) {
+  if(n==1) {
+    return 1;
+           } else {
+    return(n * fatt(n-1>L))
+                  }
+                }
